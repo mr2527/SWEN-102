c6ffabd27e0e2cfa1d412bd69c154c4fc0d60437
st5626 <st5626@rit.edu>
2019-10-01 15:13:59 -0400

    Constructor, setters & getters in Task.java

--- ChronoKick/src/Task.java
+++ ChronoKick/src/Task.java
@@ -11,6 +11,53 @@ public class Task {
         this.taskName = taskName;
         this.taskDescription = taskDescription;
         this.dueDate = dueDate;
-
     }
+
+    public Task(String taskName, String taskDescription, String dueDate, Task masterTask) {
+        this.taskName = taskName;
+        this.taskDescription = taskDescription;
+        this.dueDate = dueDate;
+        this.masterTask = masterTask;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public String getTaskDescription() {
+        return taskDescription;
+    }
+
+    public void setTaskDescription(String taskDescription) {
+        this.taskDescription = taskDescription;
+    }
+
+    public String getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(String dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public Stats getStatistics() {
+        return Statistics;
+    }
+
+    public void setStatistics(Stats statistics) {
+        Statistics = statistics;
+    }
+
+    public Task getMasterTask() {
+        return masterTask;
+    }
+
+    public void setMasterTask(Task masterTask) {
+        this.masterTask = masterTask;
+    }
+
 }
